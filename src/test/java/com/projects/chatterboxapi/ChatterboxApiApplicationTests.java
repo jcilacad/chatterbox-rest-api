97f700a972df57b5a8776c24c65900123d054ecf
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-24 19:50:15 +0800

    Initial commit.

---
+++ src/test/java/com/projects/chatterboxapi/ChatterboxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.chatterboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterboxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
