2fb2f8472cefb52f05f1dc02db52ecb9e3850766
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-24 20:07:47 +0800

    Added Room and User Entities.

---
+++ src/main/java/com/projects/chatterboxapi/entity/Room.java
@@ -0,0 +1,24 @@
+package com.projects.chatterboxapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Room {
+    private String id;
+    private String[] names;
+
+    public Room(String id, String name) {
+        this.id = id;
+        this.names = new String[1];
+        this.names[0] = name;
+    }
+
+    public Room(String id, String name1, String name2) {
+        this.id = id;
+        this.names = new String[2];
+        this.names[0] = name1;
+        this.names[1] = name2;
+    }
+}
---
+++ src/main/java/com/projects/chatterboxapi/entity/User.java
@@ -0,0 +1,14 @@
+package com.projects.chatterboxapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String username;
+    private boolean isOnline;
+}
