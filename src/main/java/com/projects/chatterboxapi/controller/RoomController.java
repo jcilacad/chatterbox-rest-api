0faf780a83b33e238982111d1a7b3aaaecc890b0
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-26 00:32:06 +0800

    Refactored controller and make used of impl package for file system.

--- src/main/java/com/projects/chatterboxapi/controller/RoomController.java
+++ src/main/java/com/projects/chatterboxapi/controller/RoomController.java
@@ -6,6 +6,7 @@ import com.projects.chatterboxapi.entity.Room;
 import com.projects.chatterboxapi.entity.User;
 import com.projects.chatterboxapi.repository.RoomRepository;
 import com.projects.chatterboxapi.repository.UserRepository;
+import com.projects.chatterboxapi.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,77 +24,17 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RoomController.class);
-    private RoomRepository roomRepository;
-    private UserRepository userRepository;
+    private RoomService roomService;
 
     @GetMapping(value = "/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Room>> getRooms(@PathVariable int userId) {
-        Set<String> roomIds = roomRepository.getUserRoomIds(userId);
-        if (roomIds == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        List<Room> rooms = new ArrayList<>();
-
-        for (String roomId : roomIds) {
-            boolean roomExists = roomRepository.isRoomExists(roomId);
-            if (roomExists){
-                String name = roomRepository.getRoomNameById(roomId);
-                if (name == null) {
-                    // private chat case
-                    Room privateRoom = handlePrivateRoomCase(roomId);
-                    if (privateRoom == null){
-                        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-                    }
-                    rooms.add(privateRoom);
-                } else {
-                    rooms.add(new Room(roomId, name));
-                }
-            }
-        }
-        return new ResponseEntity<>(rooms, HttpStatus.OK);
-    }
-
-    private String[] parseUserIds(String roomId){
-        String[] userIds = roomId.split(":");
-        if (userIds.length != 2){
-            LOGGER.error("User ids not parsed properly");
-            throw new RuntimeException("Unable to parse users ids from roomId: "+roomId);
-        }
-        return userIds;
-    }
-
-    private Room handlePrivateRoomCase(String roomId){
-        String[] userIds = parseUserIds(roomId);
-        User firstUser = userRepository.getUserById(Integer.parseInt(userIds[0]));
-        User secondUser = userRepository.getUserById(Integer.parseInt(userIds[1]));
-        if (firstUser == null || secondUser == null){
-            LOGGER.error("Users were not found by ids: "+ Arrays.toString(userIds));
-            return null;
-        }
-        return new Room(roomId, firstUser.getUsername(), secondUser.getUsername());
+       return ResponseEntity.ok(roomService.getRooms(userId));
     }
 
     @GetMapping(value = "/messages/{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Message>> getMessages(@PathVariable String roomId, @RequestParam int offset, @RequestParam int size) {
-        boolean roomExists = roomRepository.isRoomExists(roomId);
-        List<Message> messages = new ArrayList<>();
-        if (roomExists) {
-            Set<String> values = roomRepository.getMessages(roomId, offset, size);
-            for (String value : values) {
-                messages.add(deserialize(value));
-            }
-        }
-        return new ResponseEntity<>(messages, HttpStatus.OK);
-    }
-
-    private Message deserialize(String value){
-        Gson gson = new Gson();
-        try {
-            return gson.fromJson(value, Message.class);
-        } catch (Exception e) {
-            LOGGER.error(String.format("Couldn't deserialize json: %s", value), e);
-        }
-        return null;
+    public ResponseEntity<List<Message>> getMessages(@PathVariable String roomId,
+                                                     @RequestParam int offset,
+                                                     @RequestParam int size) {
+        return ResponseEntity.ok(roomService.getMessages(roomId, offset, size));
     }
 }
