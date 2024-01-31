0b77ed8cc0818fc952f2d5d8d3a577b3e2c94d06
jcilacad <johnchristopherilacad27@gmail.com>
2024-01-31 14:46:04 +0800

    Added entities for chat service.

---
+++ src/main/java/com/projects/chatterboxapi/entity/ChatNotification.java
@@ -0,0 +1,14 @@
+package com.projects.chatterboxapi.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatNotification {
+    private String id;
+    private String senderId;
+    private String senderName;
+}
---
+++ src/main/java/com/projects/chatterboxapi/entity/ChatRoom.java
@@ -0,0 +1,19 @@
+package com.projects.chatterboxapi.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class ChatRoom {
+    @Id
+    private String id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+}
